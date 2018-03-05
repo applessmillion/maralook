5f105e6be966ce34ed3a3e41491f56e621f7fd59
applessmillion <appless9009@gmail.com>
2018-03-05 00:34:33 -0600

    Misc Changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # maralook
-Welcome to the official source of Maralook.com
+Welcome to the official source for Maralook.com
+Maralook was created in the early part of 2017, and released during the summer.
 Maralook has since been made public for improvements, and can be used and shared freely following the license.
--- web/users.php
+++ web/users.php
@@ -18,9 +18,7 @@ $data_connect;
 	<body>
 		<div id="main">
 		<?php echo file_get_contents('header.html') . "</br>"; ?>
-		<img src="img/corner.png" width="9">
-		<img src="img/border.png" width="692" height="9" border="0">
-		<img src="img/corner2.png" width="9">
+		<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
 		<table align="center" width="710">
 <!-- End Init -->
 <?php
@@ -93,9 +91,7 @@ $data_connect;
 </tr>
 </table>
 </div>
-<img src="img/corner3.png" width="9" >
-<img src="img/border.png" width="692" height="9" border="0">
-<img src="img/corner4.png" width="9">
+<img src="img/corner3.png" width="9" ><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner4.png" width="9">
 </div>
 </body>
 </html>
