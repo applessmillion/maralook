65c8d4dcd759cae271e57a7896394e838a75ab4c
applessmillion <appless9009@gmail.com>
2018-03-11 11:44:51 -0400

    Cleaned up a bit
    Still messy. Still needs work. But it is an improvement.

--- web/price.php
+++ web/price.php
@@ -11,47 +11,40 @@ $con = new mysqli($ip,$user,$pw,$db);
 #CODE FOR SEARCHING DATABASE AND PRINTING RESULTS#
 if(isset($_GET["min"])) {
 ?>    
-			<html>
-			<!-- Initalize Page -->
-				<head>
-					<title>Maralook - Price Search</title>
-					<link rel="stylesheet" type="text/css" href="style.css">
-				</head>
-				<body>
-					<div id="main">
-					<?php echo file_get_contents('header.html') . "</br>"; ?>
-					<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
-					<table align="center" width="710">
-			<!-- End Init -->
+<html>
+	<!-- Initalize Page -->
+	<head>
+		<title>Maralook - Price Search</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="main">
+			<?php echo file_get_contents('header.html') . "</br>"; ?>
+			<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
+			<table align="center" width="710">
+	<!-- End Init -->
 
 <?php
+
+	//Variables needed for searching.
     $srch = $_GET["min"];
     $srch2 = $_GET["max"];
     $name = $_GET["search"];
 
-    if($srch2 < 0)
+	//Kicks in if no input for the maximum field. 500k is the max. price for MP.
+    if($srch2 < 0 OR $srch2 == NULL)
     {
         $srch2 = 5000000;
     }
-    elseif($srch2 == NULL)
-    {
-        $srch2 = 500000;
-    }
-    if($srch < 0)
+	//Kicks in if no input for the minimum field.
+    if($srch < 0 OR $srch == NULL)
     {
         $srch = 1;
     }
-    elseif($srch == NULL)
-    {
-        $srch = 1;
-    }
-    
     if($name == NULL)
     {
         $name = ' ';
     }
-    
-    #DEBUG echo $srch2 , $srch , $name;
     
     #GET NAME FROM SEARCH TERMS#
     $search_query = mysqli_query($con, "SELECT * FROM Names WHERE LastPrice>='$srch' AND LastPrice<='$srch2' AND ItemName LIKE '%$name%' ORDER BY LastPrice ASC LIMIT 50");
@@ -61,44 +54,47 @@ if(isset($_GET["min"])) {
     
     if($search_nums > 0)
     {
-    echo '<tr><th><h2>Found '. $search_nums .' items!</h2></th></tr>';
-    echo '<tr><th><hr style="border-color:#6D7ACE; width:55%;"></th></tr>';
-    while ($obj = mysqli_fetch_object($search_query)) {
-        echo "<tr><th>" . $obj->ItemName," (", $obj->LastPrice,"MP) <strong><a class='reg' href='item.php?info=" . urlencode($obj->ItemName) . "'>Maralook</a> | <a target='_blank' class='reg' href='http://www.marapets.com/shopsearch.php?do=" . $obj->ItemName . "'>Shop Search</a></strong>";
-        if($obj->SecondaryName != NULL)
-        {
-           echo "  (" . $obj->SecondaryName . ") </a><br /></th></tr>";
-        }
+		echo '<tr><th><h2>Found '. $search_nums .' items!</h2></th></tr>';
+		echo '<tr><th><hr style="border-color:#6D7ACE; width:55%;"></th></tr>';
+		while ($obj = mysqli_fetch_object($search_query)) {
+			echo "<tr><th><a class='reg' href='item.php?info=" . urlencode($obj->ItemName) . "'>" . $obj->ItemName," (", $obj->LastPrice,"MP)</a><strong> [<a target='_blank' class='reg' href='http://www.marapets.com/shopsearch.php?do=" . $obj->ItemName . "'>Shop Search</a>]</strong>";
+			if($obj->SecondaryName != NULL)
+			{
+				echo "  (" . $obj->SecondaryName . ") </a><br /></th></tr>";
+			}
         
+		}
     }
-    }
-    #Handle this before the below, because both are technically going to happen if it's over 500k
+	
+    //If the user has an amount higher than 500k, let them know why it won't work.
     elseif($srch > 500000)
-    {
-      
-    echo '<tr><th><h2>Oops!</h2></th></tr>';
-    echo '<tr><th><hr style="border-color:#6D7ACE; width:55%;"></th></tr>';
-    echo '<tr><th>It appears your minimum price was over 500,000MP. Player shops cannot price anything over 500,000MP, so we do not have anything to display!</th></tr>';      
+    { ?>
+		<tr>
+			<th>
+				<h2>Oops!</h2>
+			</th>
+		</tr>
+    <tr><th><hr style="border-color:#6D7ACE; width:55%;"></th></tr>';
+    <tr><th>It appears your minimum price was over 500,000MP. Player shops cannot price anything over 500,000MP, so we do not have anything to display!</th></tr>
+	<?php
     }
     elseif($search_nums == 0)
-    {
-?>
-    <tr>
-		<th>
-			<h2>No items were found!</h2>
-		</th>
-	</tr>
-    <tr>
-		<th>
-			<hr style="border-color:#6D7ACE; width:55%;">
-		</th>
-	</tr>
-    <tr>
-		<th>Try searching with a different minimum or maximum, because there were no items in that range!</th>
-	</tr>
-<?php
-    }
-?>
+    { ?>
+		<tr>
+			<th>
+				<h2>No items were found!</h2>
+			</th>
+		</tr>
+		<tr>
+			<th>
+				<hr style="border-color:#6D7ACE; width:55%;">
+			</th>
+		</tr>
+		<tr>
+			<th>Try searching with a different minimum or maximum, because there were no items in that range!</th>
+		</tr>
+	<?php
+    } ?>
     <tr>
 		<td style="height:20px;">
 			</br>
@@ -107,61 +103,52 @@ if(isset($_GET["min"])) {
 	</tr>
 <?php
 }
-else {
-		?>
-			<html>
-			<!-- Initalize Page -->
-				<head>
-					<title>Maralook - Search Prices</title>
-					<link rel="stylesheet" type="text/css" href="style.css">
-				</head>
-				<body>
-					<div id="main">
-					<?php echo file_get_contents('header.html') . "</br>"; ?>
-					<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
-					<table align="center" width="710">
-			<!-- End Init -->
-			
-					<tr>
-						<th>
-							<a href="price.php">
-								<img src="img/search-price.png">
-							</a>
-						</th>
-					</tr>
-					<tr>
-						<th>
-							<img src="img/titles/price-search.png">
-						</th>
-					</tr>
-					<tr>
-						<td style="height:8px" ></td>
-					</tr>
-					<tr>
-						<th>
-						<?php echo '<p>' , $price_desc , '</p>'; ?>
-						</th>
-					</tr>
-					<tr>
-						<td style="height:10px" ></td>
-					</tr>
-					<tr>
-						<th>
+else { ?>
+<html>
+	<!-- Initalize Page -->
+	<head>
+		<title>Maralook - Search Prices</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="main">
+			<?php echo file_get_contents('header.html') . "</br>"; ?>
+			<img src="img/corner.png" width="9"><img src="img/border.png" width="692" height="9" border="0"><img src="img/corner2.png" width="9">
+			<table align="center" width="710">
+	<!-- End Init -->
+				<tr>
+					<th>
+						<a href="price.php">
+							<img src="img/search-price.png">
+						</a>
+					</th>
+				</tr>
+				<tr>
+					<th>
+						<img src="img/titles/price-search.png">
+					</th>
+				</tr>
+				<tr>
+					<td style="height:8px" ></td>
+				</tr>
+				<tr>
+					<th>
+					<?php echo '<p>' , $price_desc , '</p></br></br>'; ?>
+					</th>
+				</tr>
+				<tr>
+					<th>
 						<form action="price.php" method="get"></br>
 							<strong>Minimum Price: </strong>
 							<input type="text" name="min" maxlength="8" size="8"></br></br>
 							<strong>Maximum Price: </strong><input type="text" name="max" maxlength="8" size="8"></br></br>
 							<strong>Search Term: </strong><input type="text" name="search" maxlength="34" size="16"></br></br>
 							<input type="submit" value="Search">
-						</form>
-					<tr>
-						<td style="height:26px" ></td>
-					</tr>
-					</tr></table>
-					</th></tr>
-<?php   
-}    
-?>
+						</form></br></br></br>
+					</th>
+				</tr>
+			</table>
+<?php }  ?>
 <tr>
 	<td style="height:10px"></td>
 </tr>
