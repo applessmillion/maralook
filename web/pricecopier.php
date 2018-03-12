2e446ac967487fc5da606dc16259c0bee622fcb0
applessmillion <appless9009@gmail.com>
2018-03-12 00:56:28 -0400

    Added comments for better understanding of stuff
    Cleaned up about.php as well, removing some unused mysql connection stuff.

--- web/pricecopier.php
+++ web/pricecopier.php
@@ -1,32 +1,61 @@
 <?php
+/*
+This page is mainy used for displaying item information on the pages of
+said items on Maralook. It displays the image, wanted/shop info and URLs,
+along with the other usual stuff that pricechecker.php on ML shows.
+*/
+
+//Needed for the magic happening below.
 include_once('simple_html_dom.php');
-#Grab the pricechecker page
 
+/* As this page is called from other pages mainly, an id should be set
+from the other file. If not, it'll break and tell you! */
 if(isset($_GET['id'])){
-$html = file_get_html('http://www.marapets.com/pricechecker.php?id=' . $_GET["id"] . '&marasite=1');
+	
+	//Opens the 
+	$html = file_get_html('http://www.marapets.com/pricechecker.php?id=' . $_GET["id"] . '&marasite=1');
 
-#Grab the details..
-foreach($html->find('img[border="0"]') as $picture); 
-foreach($html->find('font["size+1"] b') as $name); 
-foreach($html->find('i') as $shop); 
-foreach($html->find('font[color="#C53F55"]') as $rarity); 
-$rarityno = preg_replace("/[^0-9.]/", "", $rarity->plaintext);
-foreach($html->find('font[color="#0000FF"]') as $want); 
-$wantno = preg_replace("/[^0-9.]/", "", $want->plaintext);
-foreach($html->find('a font[color="#5F148D"]') as $playershop); 
+	//Save the picture as variable.
+	foreach($html->find('img[border="0"]') as $picture);
+
+	//Save the item name text as variable. 
+	foreach($html->find('font["size+1"] b') as $name);
+
+	//Save the shop text as a variable. 
+	foreach($html->find('i') as $shop);
+
+	//Save the rarity number as a variable. Gets rid of the R.
+	foreach($html->find('font[color="#C53F55"]') as $rarity); 
+	$rarityno = preg_replace("/[^0-9.]/", "", $rarity->plaintext);
+
+	//Save the wanted number as a variable. Gets rid of the text associated.
+	foreach($html->find('font[color="#0000FF"]') as $want); 
+	$wantno = preg_replace("/[^0-9.]/", "", $want->plaintext);
+
+	//Get the text and details for player shops.
+	foreach($html->find('a font[color="#5F148D"]') as $playershop); 
 
 
-#Finally, display it all in HTML
-echo '<table align="center" width="510">';
-echo '<tr><td width="85"><img src="'. $picture->src . '"></td></tr>';
-echo '<tr><td width="250"><b style="line-height:1.15; font-size:14pt">' , $name , '</b></br>';
-echo '<b style="color: darkorange;">Rarity ' , $rarityno , ' </b>';
-echo '<b style="color: darkblue;"><a href="http://www.marapets.com/wanted.php?do=browse&id=', $_GET["id"] , '" style="font-size:100%;">' , $wantno , ' Wanted', '</a></b></br>' , $shop , '</i></br>'; 
-echo '<a target="_blank" href="http://www.marapets.com/shopsearch.php?do=' , $name->plaintext , '" style="font-size:100%;">', $playershop , '</a></br>';
-echo '</td>';
-echo '</tr></table>';
+	/* Finally, echo it all into HTML. Not worrying about formatting as
+	it is handled by the page it is inserted into. */
+	echo '<table align="center" width="510">';
+	echo '<tr><td width="85"><img src="'. $picture->src . '"></td></tr>';
+	echo '<tr><td width="250"><b style="line-height:1.15; font-size:14pt">' , $name , '</b></br>';
+	echo '<b style="color: darkorange;">Rarity ' , $rarityno , ' </b>';
+	
+	//Display wanted text, with link to the wanted page.
+	echo '<b style="color: darkblue;"><a href="http://www.marapets.com/wanted.php?do=browse&id=', $_GET["id"] ,
+		 '" style="font-size:100%;">' , $wantno , ' Wanted', '</a></b></br>' , $shop , '</i></br>'; 
+		 
+	//Display shop text with a link to the shop search page for the item.
+	echo '<a target="_blank" href="http://www.marapets.com/shopsearch.php?do=' , $name->plaintext ,
+		 '" style="font-size:100%;">', $playershop , '</a></br>';
+	echo '</td></tr>';
+	echo '</table>';
 }
+
+//If for some reason ID is not set, handle it here.
 else{
-echo "Missing vars!";
+	echo "Missing vars!";
 }
 ?>
--- web/simplelisting.php
+++ web/simplelisting.php
@@ -1,43 +1,59 @@
 <?php
-    $pido = pcntl_fork();
-    if ($pido == 0) {
-include_once('simple_html_dom.php');
+/*
+This page is specifically used to display the items most recently
+searched for on the items.php page. Shows only the images and names,
+which are pulled by pricechecker.php with the recent items pulled
+from our own lovely database.
+Also will show a blank picture and the names of most recent items
+if MP is for some reason down or cannot be reached.
+*/
 
-	#Grab the pricechecker page
+//This will be used later down the road for timing out.
+$pido = pcntl_fork();
+if ($pido == 0) {
+	
+	//Since our other option won't scan MP, we only need this here.
+	include_once('simple_html_dom.php');
+
+	//Grab the pricechecker page from Marapets.
 	$html = file_get_html('http://www.marapets.com/pricechecker.php?id=' . $_GET["id"] . '&marasite=1');
 
-	#Grab the details..
+	//Grab the picture and name from the page.
 	foreach($html->find('img[border="0"]') as $picture); 
 	foreach($html->find('font["size+1"] b') as $name); 
 
-	#Finally, display it all in HTML
+	//Finally, echo it all in HTML
 	echo '<table align="center" width="100">';
 	echo '<tr><td width="84"><img src="'. $picture->src . '"></td></tr>';
 	echo '<tr><td><b style="font-size:8pt;color:darkblue">'.$name.'</b></br>';
-	echo '</td>';
-	echo '</tr></table>';
+	echo '</td></tr>';
+	echo '</table>';
 	
-} 
+}
+//So here's what happens if Marapets is unresponsive.
 else{
-    // this is the parent process, and we know the child process id is in $pid
-    sleep(2); // wait 3 seconds
+	
+    //This sleeps for 2 seconds. Once it wakes up, it's death to the above.
+    sleep(2); // wait 2s
+	
+	//Since we won't be using Marapets here, connect to our nifty db!
 	include_once 'config.php';
-	##################CONNECTION INFO FOR DATABASE###################
     
+	//Connection info and stuff. 
 	$con = new mysqli($ip,$user,$pw,$db);
 	$info = $_GET["id"];
     $query = mysqli_query($con, "SELECT * FROM Names WHERE ItemID='$info'");
     $obj = mysqli_fetch_object($query);
-    $iin = $obj->ItemName;
+    $itemname = $obj->ItemName;
 	
-	#Finally, display it all in HTML
+	//Display a blank image and the name retrieved from our database.
 	echo '<table align="center" width="100">';
 	echo '<tr><td width="84"><img src="http://www.maralook.com/img/buffer.png"></td></tr>';
-	echo '<tr><td><b style="font-size:8pt;color:darkblue">'.$iin.'</b></br>';
-	echo '</td>';
-	echo '</tr></table>';
+	echo '<tr><td><b style="font-size:8pt;color:darkblue">'.$itemname.'</b></br>';
+	echo '</td></tr>';
+	echo '</table>';
 	
-	
-	posix_kill($pido, SIGKILL); // then kill the child
+	//This is the knife to the if part of this if/else.
+	posix_kill($pido, SIGKILL);
 }
 ?>
