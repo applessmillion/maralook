cde6a4bac20c937620b0cdbc2d8a8d34489ccba0
applessmillion <appless9009@gmail.com>
2018-03-12 00:57:04 -0400

    Added blank config
    Simply uploaded for better understanding of what is in the file.

---
+++ web/config.php
@@ -0,0 +1,10 @@
+<?php
+/* This page is used as the holder of mySQL connection info.
+For obvious reasons, the fields are left blank on this project.
+It is simply here now to show the variables and such.
+*/
+$ip   = ""; //MySQL Server IP
+$user = ""; //MySQL user
+$pw   = ""; //MySQL password
+$db   = ""; //Database
+?>
